d8683fd303c14c011e59c523dbb1d63479bd8a6e
shesaidimnothing <kidslove3play@gmail.com>
2025-03-25 11:14:36 +0100

    dernière version j'espère, essayé de résoudre les conflits vercel

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,14 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma;
+// PrismaClient est attaché à l'objet global en développement pour éviter
+// d'épuiser la limite de connexions à la base de données pendant le hot-reloading
+const globalForPrisma = global;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma; 
